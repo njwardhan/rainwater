fca18bfffe2cfff4490bee68a28c8751caa55e47
Nishant Joywardhan <njwardhan@gmail.com>
2022-09-14 16:30:22 +0530

    main app frontend basics

--- src/frontend/pages/launch-app.js
+++ src/frontend/pages/launch-app.js
@@ -1,7 +1,23 @@
+import { Button } from 'react-bootstrap';
+
 const Launch_App = () => {
+    function refreshPage() {
+      window.location.reload(false);
+    }
     document.title = 'App | RainWater Protocol';
-    return (    
-      <h1>Welcome to RainWater Protocol</h1>
+    return (
+      <div className="body2">
+        <div className="Top-bar">
+          <h1 className="App-name-2" onClick={refreshPage}>RainWater</h1>
+          <button className="Wallet-button">Connect Wallet</button>
+        </div>
+        <div className="Upload">
+          <h2 className="Image-upload">Share Image</h2>
+          <button className="Upload-button">Choose File</button>
+          <input type="text" name="Description" placeholder="Image Description .." />
+          <button className="Post">Upload</button>
+        </div>
+      </div>
     );
   };
   
