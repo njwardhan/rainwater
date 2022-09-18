b80f5e380f4e38b5ca013f5086134bafc5beecbc
Nishant Joywardhan <njwardhan@gmail.com>
2022-09-19 01:47:57 +0530

    testing complete

--- src/backend/test/rainwater.js
+++ src/backend/test/rainwater.js
@@ -11,7 +11,7 @@ describe("RainWater", function () {
         const rainwater = await ethers.getContractFactory("rainwater");
         [deployer, author, tipper] = await ethers.getSigners();
         contract = await rainwater.deploy();
-    });
+    })
 
     describe("Deployment", function() {
         it("deploys successfully", async function () {
@@ -46,7 +46,6 @@ describe("RainWater", function () {
 
         it("creates images", async function() {
             // Successful creation of an image, tallied via event log
-            // let result = await contract.connect(author).uploadImage(hash, ImageDescription);
             let eventLog = await (await result.wait()).events[0]
             expect(eventLog.args[0].toNumber()).to.equal(1, 'correct image id');
             expect(eventLog.args[1]).to.equal(hash, 'correct image hash');
@@ -67,7 +66,40 @@ describe("RainWater", function () {
     });
 
     describe("Tipping", function() {
-       //
+        let tip, existence;
+        let tipId = 1;
+
+        it("tipping the wrong image id", async function () {
+            await expect (contract.connect(tipper).tipImageAuthor(99)).to.be.reverted;
+        });
+
+        it("tip successful", async function () {
+            // Image to be tipped is created
+            existence = await contract.connect(author).uploadImage('test hash', 'test description');
+
+            // Retrieve the old balance of the image author
+            let oldBalance = await author.getBalance();
+            oldBalance = ethers.BigNumber.from(oldBalance);
+
+            // Successfully tip the author
+            tip = await contract.connect(tipper).tipImageAuthor(tipId, { value: ethers.utils.parseEther("0.1") });
+            let eventLog = await (await tip.wait()).events[0]
+            expect(eventLog.args[0].toNumber()).to.equal(tipId, 'correct image id');
+            expect(eventLog.args[1]).to.equal('test hash', 'correct image hash');
+            expect(eventLog.args[2]).to.equal('test description', 'correct image description');
+            // expect(eventLog.args[3].toNumber()).to.equal('100000000000000000', 'correct tip amount');
+            expect(eventLog.args[4]).to.equal(author.address, 'correct author');
+            
+            let tippedAmount = eventLog.args[3];
+            tippedAmount = ethers.BigNumber.from(tippedAmount);
+
+            // Retrieve the new balance of the image author
+            let newBalance = await author.getBalance();
+            newBalance = ethers.BigNumber.from(newBalance);
+
+            // Assert that the fund has indeed been transferred to the author
+            expect(oldBalance.add(tippedAmount)).to.equal(newBalance);
+        });
     });
-}) 
+});
 
