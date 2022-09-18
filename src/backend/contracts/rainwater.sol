04681ae3828478804c08bc8ec3958f24e7222204
Nishant Joywardhan <njwardhan@gmail.com>
2022-09-18 23:35:22 +0530

    business logic full, testing almost

--- src/backend/contracts/rainwater.sol
+++ src/backend/contracts/rainwater.sol
@@ -5,4 +5,74 @@ import "hardhat/console.sol";
 
 contract rainwater {
     string public name = "RainWater";
+    uint public imageCount = 0;
+
+    // Store Images
+    mapping(uint => Image) public images;
+    
+    // database to store data of each image entry
+    struct Image { 
+        uint id; // id of each uploaded image
+        string hash; // IPFS hash for each image
+        string description; // Description about the image
+        uint tipAmount; // the amount of tip associated with that image
+        address payable author; // the creator of the image post who might get tipped for it
+    }
+
+    // Event to take a note whenever a new image is created
+    event ImageCreated (  
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    event ImageTipped (
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    // Create Images
+    function uploadImage(string memory _hash, string memory _description) external payable {
+        // Some key requirements that the hash or description is not empty and that the uploader is valid
+        require(bytes(_hash).length > 0);
+        require(bytes(_description).length > 0);
+        require(msg.sender != address(0x0));
+        
+        // Increment the image count
+        imageCount++;
+        
+        // Add image to the contract
+        images[imageCount] = Image(imageCount, _hash, _description, 0, payable(msg.sender));  
+
+        // Trigger the event on image creation
+        emit ImageCreated(imageCount, _hash, _description, 0, payable(msg.sender));
+    }
+
+    // Tip Images
+    function tipImageAuthor(uint _id) public payable {
+        // Require that the id a valid one
+        require(_id > 0 && _id <= imageCount);
+
+        // Fetch the image that has to be tipped
+        Image memory _image = images[_id];
+
+        // msg.value denotes the amount of Ether that was sent in while giving a call to THIS FUNCTION.
+        // So that whole amount is to be transferred to the author of the image
+        address payable _author = _image.author;
+        _author.transfer(msg.value);
+
+        // Upgrade the tip amount associated with the fetched image
+        _image.tipAmount += msg.value;
+
+        // Update the image back into the mapping
+        images[_id] = _image;
+
+        // Trigger the event when image is tipped
+        emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+    }
 }
--- src/backend/test/rainwater.js
+++ src/backend/test/rainwater.js
@@ -1,13 +1,73 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { assertHardhatInvariant } = require("hardhat/internal/core/errors");
 
-describe("Smoke Test", function () {
-    it("Should display the correct name", async function () {
-        // Create the smart contract object to test from
-        const TestContract = await ethers.getContractFactory("rainwater");
-        const contract = await TestContract.deploy();
+describe("RainWater", function () {
+    let contract;
+    let deployer, author, tipper;
 
-        // Get output from functions
-        const name = await contract.name();
-        expect(name).to.equal("RainWater");
+    beforeEach(async () => {
+        // Create the smart contract object to test from, also get signers
+        const rainwater = await ethers.getContractFactory("rainwater");
+        [deployer, author, tipper] = await ethers.getSigners();
+        contract = await rainwater.deploy();
     });
-});
+
+    describe("Deployment", function() {
+        it("deploys successfully", async function () {
+            const address = await contract.address;
+            expect(address).to.not.equal(0x0);
+            expect(address).to.not.equal('');
+            expect(address).to.not.equal(null);
+            expect(address).to.not.equal(undefined);
+        });
+    
+        it("has a name", async function () {
+            const name = await contract.name();
+            expect(name).to.equal("RainWater");
+        });
+    });
+
+    describe("Images", function () {
+        const hash = 'abc12345njw';
+        const ImageDescription = 'Hello World!';
+        let result, count;
+
+        beforeEach(async() => {
+            result = await contract.connect(author).uploadImage(hash, ImageDescription);
+            count = await contract.imageCount();
+        })
+        
+        it("requirements for image creation", async function() {
+            // Failure cases where hash or description empty, caught
+            await expect (contract.connect(author).uploadImage('', ImageDescription)).to.be.reverted;
+            await expect (contract.connect(author).uploadImage(hash, '')).to.be.reverted;
+        });
+
+        it("creates images", async function() {
+            // Successful creation of an image, tallied via event log
+            // let result = await contract.connect(author).uploadImage(hash, ImageDescription);
+            let eventLog = await (await result.wait()).events[0]
+            expect(eventLog.args[0].toNumber()).to.equal(1, 'correct image id');
+            expect(eventLog.args[1]).to.equal(hash, 'correct image hash');
+            expect(eventLog.args[2]).to.equal(ImageDescription, 'correct image description');
+            expect(eventLog.args[3].toNumber()).to.equal(0, 'correct tip amount');
+            expect(eventLog.args[4]).to.equal(author.address, 'correct author');
+        });
+
+        it("lists images", async function () {
+            // Lists the item from the stored image mapping
+            const image = await contract.images(count); 
+            expect(image.id.toNumber()).to.equal(1, 'correct image id');
+            expect(image.hash).to.equal(hash, 'correct image hash');
+            expect(image.description).to.equal(ImageDescription, 'correct image description');
+            expect(image.tipAmount.toNumber()).to.equal(0, 'correct tip amount');
+            expect(image.author).to.equal(author.address, 'correct author');
+        });
+    });
+
+    describe("Tipping", function() {
+       //
+    });
+}) 
+
