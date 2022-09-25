b61c879e10d576045b53765814cc5360ad514452
Nishant Joywardhan <njwardhan@gmail.com>
2022-09-25 12:44:19 +0530

    state array handled better, frontend cleanup

--- src/frontend/pages/launch-app.js
+++ src/frontend/pages/launch-app.js
@@ -30,13 +30,14 @@ class Launch_App extends Component {
     this.setState({ rainwater: contract });
     // console.log(contract.address);
     const count = await contract.functions.imageCount();
-    console.log(count)
+    console.log(parseInt(count))
 
     // Load Images
+    let copy = [];
     for (var i = 1; i <= count; i++) {
-      console.log("hello");
       const image = await contract.functions.images(i);
-      this.setState({ images: [...this.state.images, image] });
+      copy.push(image);
+      this.setState({ images: copy });
     }
 
     // Sort images as per the tip
@@ -186,81 +187,20 @@ class Launch_App extends Component {
             </div>
           </div>
         <div className="column">
-          *due to the slow Pinata service, it might take time to show images in app
+          <p style={{
+            display: 'flex',
+            justifyContent: 'center',
+            paddingTop: '30px',
+            }}>
+            *images might take some time to show up
+          </p>
         </div>
       </div>
-
 
       // <div className="container-fluid-mt-5">
       //   <div className="row">
       //     <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
       //       <div className="content mr-auto ml-auto">
-              // <div className="Uploader">
-              //   {this.state.loading
-              //     ? <div id="loader" className='text center mt-5'><p>Loading...</p></div>
-              //     : <div className='items'>
-              //       <h2 style={{ color: "black" }}>Share Image</h2>
-              //       <form onSubmit={(event) => {
-              //         event.preventDefault()
-              //         const description = this.imageDescription.value
-              //         // this.uploadImage(description)
-              //         this.sendFileToIPFS(description)
-              //       }} >
-              //         <input type='file' accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.captureFile} />
-              //         <div className="form-group mr-sm-2">
-              //           <br></br>
-              //           <input
-              //             id="imageDescription"
-              //             type="text"
-              //             ref={(input) => { this.imageDescription = input }}
-              //             className="form-control"
-              //             placeholder="Image description..."
-              //             required />
-              //         </div>
-              //         <button type="submit" className="btn btn-primary btn-block btn-lg">Upload!</button>
-              //       </form>
-              //     </div>
-              //   }
-              // </div>
-              // <div className='Main-content'>
-              //   {this.state.images.map((image, key) => {
-              //     return (
-              //       <div className="card mb-4" key={key} >
-              //         <div className="card-header">
-              //           <img
-              //             className='mr-2'
-              //             width='30'
-              //             height='30'
-              //             src={`data:image/png;base64,${new Identicon(image.author, 30).toString()}`}
-              //           />
-              //           <small className="text-muted">{image.author}</small>
-              //         </div>
-              //         <ul id="imageList" className="list-group list-group-flush">
-              //           <li className="list-group-item">
-              //             <p className="text-center"><img src={`https://gateway.pinata.cloud/ipfs/${image.hash}`} style={{ maxWidth: '420px' }} /></p>
-              //             <p>{image.description}</p>
-              //           </li>
-              //           <li key={key} className="list-group-item py-2">
-              //             <small className="float-left mt-1 text-muted">
-              //               TIPS: {ethers.utils.formatEther(image.tipAmount.toString(), 'Ether')} ETH
-              //             </small>
-              //             <button
-              //               className="btn btn-link btn-sm float-right pt-0"
-              //               name={image.id}
-              //               onClick={(event) => {
-              //                 let tipAmount = ethers.utils.parseUnits('0.000000000000000001', 18).toString();
-              //                 console.log(event.target.name, tipAmount)
-              //                 this.tipImageAuthor(event.target.name, tipAmount)
-              //               }}
-              //             >
-              //               TIP 0.1 ETH
-              //             </button>
-              //           </li>
-              //         </ul>
-              //       </div>
-              //     )
-              //   })}
-              // </div>
       //       </div>
       //     </main>
       //   </div>
