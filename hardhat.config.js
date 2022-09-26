b2366782197f9fc235df727c4d9125d6b938867a
Nishant Joywardhan <njwardhan@gmail.com>
2022-09-26 09:25:19 +0530

    Goerli testnet deployment via Infura

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 // require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.GETH_PRIVATE_KEY]
+    }
+  },
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
